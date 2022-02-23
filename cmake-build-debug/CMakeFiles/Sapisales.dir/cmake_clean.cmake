e7ea8cef9fb0d08fa7e83f034c044a4fc5543806
ZTB69 <59119404+ZTB69@users.noreply.github.com>
2022-02-23 13:06:57 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Sapisales.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sapisales.dir/main.c.obj"
+  "Sapisales.exe"
+  "Sapisales.exe.manifest"
+  "Sapisales.pdb"
+  "libSapisales.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sapisales.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Sapisales.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Sapisales.
+# This may be replaced when dependencies are built.
